d0245087814962d162845727e739cacc93d5a7b5
Mario Escorcia <mario.escorcia@aspsols.com>
2025-08-02 16:09:38 -0500

    update
    improving home styles and adding configs to deck GL map

--- src/components/Graphs/LineLayerMap/LineLayerMap.tsx
+++ src/components/Graphs/LineLayerMap/LineLayerMap.tsx
@@ -1,56 +1,80 @@
-import { DeckGL } from '@deck.gl/react';
-import type { MapViewState } from '@deck.gl/core';
-import { LineLayer } from '@deck.gl/layers';
-import Map from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import { useEffect, useState } from 'react';
+import { DeckGL } from "@deck.gl/react";
+import type { MapViewState } from "@deck.gl/core";
+import { LineLayer, ScatterplotLayer } from "@deck.gl/layers";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { useEffect, useState } from "react";
 
 const INITIAL_VIEW_STATE: MapViewState = {
-    longitude: -122.41669,
-    latitude: 37.7853,
-    zoom: 5
+  latitude: 51.5074,
+  longitude: 0.1278,
+  zoom: 7.5,
+  pitch: 10,
+  maxZoom: 15,
 };
 
-type DataType = {
-    from: [longitude: number, latitude: number];
-    to: [longitude: number, latitude: number];
-};
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
 export const LineLayerMap = () => {
-    const [data, setData] = useState<DataType[]>([]);
+  const [routes, setRoutes] = useState<any[]>([]);
+  const [airports, setAirports] = useState<any[]>([]);
 
-    useEffect(() => {
-        fetch('https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/line-data.json')
-            .then(res => res.json())
-            .then(setData)
-            .catch(console.error);
-    }, []);
-    const layers = [
-        new LineLayer<DataType>({
-            id: 'line-layer',
-            data: data,
-            getSourcePosition: (d: DataType) => d.from,
-            getTargetPosition: (d: DataType) => d.to,
-        })
-    ];
-    return (
-        <div className="lineLineLayerMap"
-            style={{
-                width : "100%",
-                height : "100%",
-                position : "relative"
-            }}
-        >
-            <DeckGL
-                initialViewState={INITIAL_VIEW_STATE}
-                controller={true}
-                layers={layers}
-                
-            >
-                <Map
-                    mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-                />
-            </DeckGL>
-        </div>
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/line/heathrow-flights.json"
     )
-}
+      .then((res) => res.json())
+      .then(setRoutes)
+      .catch(console.error);
+
+    fetch(
+      "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/line/airports.json"
+    )
+      .then((res) => res.json())
+      .then(setAirports)
+      .catch(console.error);
+  }, []);
+
+  const layers = [
+    new LineLayer({
+      id: "flight-paths",
+      data: routes,
+      getSourcePosition: (d) => d.start,
+      getTargetPosition: (d) => d.end,
+      getColor: (d) => {
+        const z = d.start[2];
+        const r = z / 10000;
+        return [255 * (1 - r * 2), 128 * r, 255 * r, 255 * (1 - r)];
+      },
+      getWidth: 1,
+      pickable: true,
+    }),
+    new ScatterplotLayer({
+      id: "airports",
+      data: airports,
+      getPosition: (d) => d.coordinates,
+      getFillColor: [255, 140, 0],
+      getRadius: 50,
+    }),
+  ];
+
+  return (
+    <div
+      className="lineLineLayerMap"
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={false}
+        layers={layers}
+      >
+        <Map mapStyle={MAP_STYLE} />
+      </DeckGL>
+    </div>
+  );
+};
--- src/components/HomeData/HomeData.tsx
+++ src/components/HomeData/HomeData.tsx
@@ -1,14 +1,170 @@
+import { AiOutlineDropbox, AiOutlineUser } from "react-icons/ai";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { VscError } from "react-icons/vsc";
 import { LineLayerMap } from "../Graphs/LineLayerMap/LineLayerMap";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 import "./homeData.css";
+
 export const HomeData = () => {
+  const generateRadomNumber = () => {
+    const number: number = Math.ceil(Math.random() * 1000);
+    return number;
+  };
+  const generateRadomProfitNumber = () => {
+    const number: number = Math.ceil(Math.random() * 100);
+    return number;
+  };
+
+  const gaugeSettings = {
+    width: 250,
+    height: 250,
+  };
+
   return (
     <section className="home-data">
-     <div style={{
-      width : "30%",
-      height : "40%"
-     }}>
-      <LineLayerMap></LineLayerMap>
-     </div>
+      <section className="home-data-cont-top">
+        <div className="glass-bg">
+          <div>
+            <span
+              style={{
+                width: "1.8rem",
+                height: "1.8rem",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              className="home-data-cont-top-icon"
+            >
+              <AiOutlineDropbox color="var(--txt-color)" />
+            </span>
+          </div>
+          <div>
+            <p>shipements made this month</p>
+            <span>
+              <p>total shipments</p>
+              {generateRadomNumber()}k
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <span
+              style={{
+                width: "1.8rem",
+                height: "1.8rem",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              className="home-data-cont-top-icon"
+            >
+              <IoIosCheckmarkCircleOutline color="var(--txt-color)" />
+            </span>
+          </div>
+          <div>
+            <p>shipements completed this month</p>
+            <span>
+              <p>completed shipments</p>
+              {generateRadomNumber()}k
+            </span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <span
+              style={{
+                width: "1.8rem",
+                height: "1.8rem",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              className="home-data-cont-top-icon"
+            >
+              <AiOutlineUser color="var(--txt-color)" />
+            </span>
+          </div>
+          <div>
+            <p> new users this month</p>
+            <span>
+              <p>total users</p>
+              {generateRadomNumber()}k
+            </span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <span
+              style={{
+                width: "1.8rem",
+                height: "1.8rem",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              className="home-data-cont-top-icon"
+            >
+              <VscError color="var(--txt-color)" />
+            </span>
+          </div>
+          <div>
+            <p> returns made this month</p>
+            <span>
+              <p>total returns</p>
+              {generateRadomNumber()}k
+            </span>
+          </div>
+        </div>
+      </section>
+      <section className="home-data-cont-middle">
+        <div
+          style={{
+            width: "75%",
+            padding : "0"
+          }}
+        >
+          <p>Live Shipment Tracking</p>
+          <LineLayerMap></LineLayerMap>
+        </div>
+        <div
+          style={{
+            width: "25%",
+          }}
+        >
+          <p>profit margin</p>
+
+          <div>
+            <Gauge
+              {...gaugeSettings}
+              value={generateRadomProfitNumber()}
+              sx={{
+                ["& .MuiGauge-valueText"]: {
+                  fontSize: 40,
+                  transform: "translate(0px, 0px)",
+                  fill: "var(--txt-color) !important",
+                },
+                [`& .${gaugeClasses.valueArc}`]: {
+                  fill: "var(--purple)",
+                },
+              }}
+              text={({ value }) => `${value}%`}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="home-data-cont-bottom">
+        <div
+          style={{
+            width: "35%",
+          }}
+        >
+          error sumary
+        </div>
+        <div
+          style={{
+            width: "65%",
+          }}
+        >
+          recent shipments
+        </div>
+      </section>
     </section>
-  )
-}
+  );
+};
